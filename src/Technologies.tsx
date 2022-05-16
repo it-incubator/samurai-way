f627aa01e133c88bcbf93de31d919b160b1a7bc5
Anton Metlitsky <ticket1201@gmail.com>
2022-05-16 21:55:34 +0300

    feat: implement Technologies component;

---
+++ src/Technologies.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Technologies = () => {
+  return (
+    <div>
+      <ul>
+        <li>html</li>
+        <li>css</li>
+        <li>js</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Technologies;
