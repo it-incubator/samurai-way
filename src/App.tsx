26cc87b1c96a9d83807f5282c453c0410214db04
Anton Metlitsky <ticket1201@gmail.com>
2022-05-26 21:54:05 +0300

    feat: add Profile component, className to app-wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import './App.css';
-import Header from './Header';
-import Technologies from './Technologies';
+import {Header} from './components/Header/Header';
+import {Profile} from './components/Profile/Profile';
 
 const App = () => {
-  return (
-    <div>
-      <Header />
-      <Technologies />
-    </div>
-  );
+    return (
+        <div className={'app-wrapper'}>
+            <Header/>
+            <Profile/>
+        </div>
+    );
 }
 
 
