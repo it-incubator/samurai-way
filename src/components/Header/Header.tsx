c7674d12d7bfa1ce71161c9d37852ac0a110e436
Anton Metlitsky <ticket1201@gmail.com>
2022-05-26 21:54:24 +0300

    feat: implement Header module

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Nav} from '../Navbar/Navbar';
+import styles from './Header.module.css';
+
+export const Header = () => {
+    return(
+        <header className={styles.header}>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/4/4e/Playstation_logo_colour.svg" alt="logo"/>
+            <Nav/>
+        </header>
+    )
+}
