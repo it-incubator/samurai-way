e714c28508930a7466a65d68383f5e08fae82a9c
Anton Metlitsky <ticket1201@gmail.com>
2022-05-27 22:06:47 +0300

    refactor: add message, likeCount props to post component

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -4,17 +4,15 @@ import {Post} from './Post/Post';
 
 export const MyPosts = () => {
     return (
-        <div>
+        <div className={styles.posts}>
             My posts
             <div>
                 <textarea name="" id=""></textarea>
                 <button>Submit</button>
             </div>
             <div className={'posts'}>
-                <Post/>
-                <Post/>
-                <Post/>
-                <Post/>
+                <Post message={'Hi, how are you?'} likeCount={15}/>
+                <Post message={`It's my first post`} likeCount={1}/>
             </div>
         </div>
     )
