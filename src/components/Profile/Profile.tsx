0bff60b178564286dce3948fa26f36a3e2d5c0b8
Anton Metlitsky <ticket1201@gmail.com>
2022-06-04 20:37:31 +0300

    refactor: made profileInfo as a component

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import styles from './Profile.module.css'
+import styles from './Profile.module.scss'
 import {MyPosts} from './MyPosts/MyPosts';
+import {ProfileInfo} from './ProfileInfo/ProfileInfo';
+
 
 export const Profile = () => {
     return (
         <div className={styles.main}>
-            <img
-                src="https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300"
-                alt="Landscape"/>
-            <div>ava+descr</div>
+            <ProfileInfo description={'Ava'} imgAlt={'Landscape'}
+                         imgUrl={'https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300'}/>
             <MyPosts/>
 
         </div>
