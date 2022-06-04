b14b4f652e86df2628120f2a084c635ba39f9afa
Anton Metlitsky <ticket1201@gmail.com>
2022-06-04 20:34:31 +0300

    feat: add route to components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import {Header} from './components/Header/Header';
 import {Profile} from './components/Profile/Profile';
+import {Dialogs} from './components/Dialogs/Dialogs';
+import {BrowserRouter, Route} from 'react-router-dom';
+import {Music} from './components/Music/Music';
+import {News} from './components/News/News';
+import {Settings} from './components/Settings/Settings';
 
 const App = () => {
     return (
-        <div className={'app-wrapper'}>
-            <Header/>
-            <Profile/>
-        </div>
+        <BrowserRouter>
+            <div className={'app-wrapper'}>
+                <Header/>
+                <div className={'content'}>
+                    <Route path={'/dialogs'} component={Dialogs}/>
+                    <Route path={'/profile'} component={Profile}/>
+                    <Route path={'/news'} component={News}/>
+                    <Route path={'/music'} component={Music}/>
+                    <Route path={'/settings'} component={Settings}/>
+                </div>
+            </div>
+        </BrowserRouter>
     );
 }
 
