322dbf3c564349923742c9ad3ed48973edb55b60
Anton Metlitsky <ticket1201@gmail.com>
2022-06-11 16:03:39 +0300

    feat: add appState and import it

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
+import {state} from './redux/state';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
+ReactDOM.render(<App appState={state}/>, document.getElementById('root'));
