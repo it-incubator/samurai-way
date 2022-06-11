0ba2c455bc145939e14d58a5a616a37afeb0976f
Anton Metlitsky <ticket1201@gmail.com>
2022-06-11 16:07:44 +0300

    feat: imp props from MyPosts.

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import styles from './Profile.module.scss'
-import {MyPosts} from './MyPosts/MyPosts';
+import {MyPosts, MyPostsPropsType} from './MyPosts/MyPosts';
 import {ProfileInfo} from './ProfileInfo/ProfileInfo';
 
 
-export const Profile = () => {
+
+
+export const Profile = (props: MyPostsPropsType) => {
     return (
         <div className={styles.main}>
             <ProfileInfo description={'Ava'} imgAlt={'Landscape'}
                          imgUrl={'https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300'}/>
-            <MyPosts/>
+            <MyPosts postsState={props.postsState}/>
 
         </div>
     )
