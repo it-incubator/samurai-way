93f4be6190ea6e2b05e8fb4b9794b270bf6da730
Anton Metlitsky <ticket1201@gmail.com>
2022-06-19 15:55:02 +0300

    feat: add props for addpost func

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './Profile.module.scss'
 import {MyPosts, MyPostsPropsType} from './MyPosts/MyPosts';
 import {ProfileInfo} from './ProfileInfo/ProfileInfo';
+import {changeNewText} from '../../redux/state';
 
 
 
@@ -11,7 +12,7 @@ export const Profile = (props: MyPostsPropsType) => {
         <div className={styles.main}>
             <ProfileInfo description={'Ava'} imgAlt={'Landscape'}
                          imgUrl={'https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300'}/>
-            <MyPosts postsState={props.postsState}/>
+            <MyPosts postsState={props.postsState} addPost={props.addPost} newMessage={props.newMessage} changeNewText={changeNewText}/>
 
         </div>
     )
