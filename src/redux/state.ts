fdacea46236544c890f46007e158534655b0d9cf
Anton Metlitsky <ticket1201@gmail.com>
2022-06-19 15:51:41 +0300

    feat: int state

--- src/redux/state.ts
+++ src/redux/state.ts
@@ -1,14 +1,20 @@
+import {renderEntireTree} from '../render';
+
 export type stateType = {
-    postsState: PostsStateType
-    dialogsState: DialogsStateType
+    profilePage: {
+        posts: PostsType
+        messageForNewPost: string
+    }
+    dialogsPage: DialogsPageType
+
 }
-export type PostsStateType = PostItemStateType[]
+export type PostsType = PostItemStateType[]
 type PostItemStateType = {
     id: number
     message: string
     likeCount: number
 }
-export type DialogsStateType = {
+export type DialogsPageType = {
     names: NamesItemType[]
     messages: MessageItemType[]
 }
@@ -19,12 +25,13 @@ type NamesItemType = {
 type MessageItemType = {
     id: number
     message: string
+    time: string
 }
 
 
 export const state: stateType = {
+    dialogsPage: {
 
-    dialogsState: {
         names: [
             {id: 1, name: 'Dimych'},
             {id: 2, name: 'Sanya'},
@@ -34,15 +41,36 @@ export const state: stateType = {
             {id: 6, name: 'Viktor'},
         ],
         messages: [
-            {id: 1, message: 'Hey'},
-            {id: 2, message: 'Wats up?'},
-            {id: 3, message: '????'},
+            {id: 1, message: 'Hey', time: '13:15'},
+            {id: 2, message: 'Wats up?', time: '13:20'},
+            {id: 3, message: '????', time: '13:30'},
         ]
+
     },
 
-    postsState: [
-        {id: 1, message: 'Hi, how are you?', likeCount: 15},
-        {id: 2, message: 'It\'s my first post', likeCount: 1},
-        {id: 3, message: 'Hey ho lets go', likeCount: 1}
-    ]
+
+    profilePage: {
+        messageForNewPost: '',
+        posts: [
+            {id: 1, message: 'Hi, how are you?', likeCount: 15},
+            {id: 2, message: 'It\'s my first post', likeCount: 1},
+            {id: 3, message: 'Hey ho lets go', likeCount: 1}
+        ]
+    }
+
+}
+
+export const addPost = (postMessage:string) => {
+    let newPost: PostItemStateType = {
+        id: 5,
+        message:postMessage,
+        likeCount:0
+    }
+    state.profilePage.posts.push(newPost)
+    renderEntireTree(state)
+}
+
+export const changeNewText = (newText:string)=>{
+    state.profilePage.messageForNewPost = newText;
+    renderEntireTree(state)
 }
