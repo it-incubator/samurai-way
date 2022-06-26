53c6135f516a159d86b5eef3f6a436b85936aced
Anton Metlitsky <ticket1201@gmail.com>
2022-06-26 16:06:33 +0300

    feat: implement oop store, del render and ref index

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,17 @@
 import './index.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import App from './App';
+import {BrowserRouter} from 'react-router-dom';
+import store from './redux/state';
 
-import {renderEntireTree} from './render';
-import {state} from './redux/state';
+const renderEntireTree = () => {
+    ReactDOM.render(<BrowserRouter>
+        <App store={store}/>
+    </BrowserRouter>, document.getElementById('root'));
+}
 
+renderEntireTree()
 
-renderEntireTree(state)
-
+store.subscribe(renderEntireTree)
--- src/redux/state.ts
+++ src/redux/state.ts
@@ -1,4 +1,11 @@
-import {renderEntireTree} from '../render';
+export type storeType = {
+    _state: stateType
+    changeNewText: (newText: string) => void
+    addPost: () => void
+    _onChange: () => void
+    subscribe: (callback: () => void) => void
+    getState: () => stateType
+}
 
 export type stateType = {
     profilePage: {
@@ -29,48 +36,61 @@ type MessageItemType = {
 }
 
 
-export const state: stateType = {
-    dialogsPage: {
+const store: storeType = {
+    _state: {
+        dialogsPage: {
 
-        names: [
-            {id: 1, name: 'Dimych'},
-            {id: 2, name: 'Sanya'},
-            {id: 3, name: 'Olya'},
-            {id: 4, name: 'Valera'},
-            {id: 5, name: 'Valera'},
-            {id: 6, name: 'Viktor'},
-        ],
-        messages: [
-            {id: 1, message: 'Hey', time: '13:15'},
-            {id: 2, message: 'Wats up?', time: '13:20'},
-            {id: 3, message: '????', time: '13:30'},
-        ]
+            names: [
+                {id: 1, name: 'Dimych'},
+                {id: 2, name: 'Sanya'},
+                {id: 3, name: 'Olya'},
+                {id: 4, name: 'Valera'},
+                {id: 5, name: 'Valera'},
+                {id: 6, name: 'Viktor'},
+            ],
+            messages: [
+                {id: 1, message: 'Hey', time: '13:15'},
+                {id: 2, message: 'Wats up?', time: '13:20'},
+                {id: 3, message: '????', time: '13:30'},
+            ]
+
+        },
+
+
+        profilePage: {
+            messageForNewPost: '',
+            posts: [
+                {id: 1, message: 'Hi, how are you?', likeCount: 15},
+                {id: 2, message: 'It\'s my first post', likeCount: 1},
+                {id: 3, message: 'Hey ho lets go', likeCount: 1}
+            ]
+        }
 
     },
+    changeNewText(newText: string) {
+        this._state.profilePage.messageForNewPost = newText;
+        this._onChange()
+    },
+    addPost() {
+        let newPost: PostItemStateType = {
+            id: 5,
+            message: this._state.profilePage.messageForNewPost,
+            likeCount: 0
+        }
+        this._state.profilePage.posts.push(newPost)
+        this._state.profilePage.messageForNewPost = '';
+        this._onChange()
+    },
+    _onChange() {
+        console.log('state changed')
+    },
 
-
-    profilePage: {
-        messageForNewPost: '',
-        posts: [
-            {id: 1, message: 'Hi, how are you?', likeCount: 15},
-            {id: 2, message: 'It\'s my first post', likeCount: 1},
-            {id: 3, message: 'Hey ho lets go', likeCount: 1}
-        ]
+    subscribe(callback: () => void) {
+        this._onChange = callback
+    },
+    getState() {
+        return this._state
     }
-
 }
 
-export const addPost = (postMessage:string) => {
-    let newPost: PostItemStateType = {
-        id: 5,
-        message:postMessage,
-        likeCount:0
-    }
-    state.profilePage.posts.push(newPost)
-    renderEntireTree(state)
-}
-
-export const changeNewText = (newText:string)=>{
-    state.profilePage.messageForNewPost = newText;
-    renderEntireTree(state)
-}
+export default store
