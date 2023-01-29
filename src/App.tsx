d75f71cd0da2ddb9a0fc3a8547305358dc663d6f
Artyom <50843331+PetukhovArt@users.noreply.github.com>
2023-01-29 16:31:15 +0500

    init: start SN project

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Technologies from './components/Technologies';
 
-function App() {
+
+const App = () => {
+  console.log("App render");
   return (
-    <div className="App">
-     Hello, samurai! Let's go!
+    <div className='app'>
+      <Header value={true} />
+      <Technologies />
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/Technologies.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Technologies = () => {
+    return <ul>
+        <li>html</li>
+        <li>css</li>
+        <li>js</li>
+        <li>react</li>
+    </ul>;
+};
+
+export default Technologies;
