af9fb95027f1d380795bb81026a95221b47c7712
alexeyYashin <alexeyyashin@mail.ru>
2023-02-25 23:32:08 +0400

    video07

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,34 @@ import './App.css';
 
 const App = () => {
   return (
-    <div className="App">
-      <ul>
-        <li>css</li>
-        <li>html</li>
-        <li>js</li>
-        <li>React</li>
-      </ul>
+    <div>
+      <Header />
+      <Technologies />
     </div>
-  );
+  )
+}
+
+const Technologies = () => {
+  return (
+    <ul>
+      <li>css</li>
+      <li>html</li>
+      <li>js</li>
+      <li>React</li>
+    </ul>
+  )
+  
+}
+
+const Header = () => {
+  return (
+    <div>
+      <a href='#'>Home</a>
+      <a href='#'>News</a>
+      <a href='#'>Feed</a>
+      <a href='#'>Messages</a>
+    </div>
+  )
 }
 
 export default App;
