e1396ba59502d32fc92cee92fb012f037244d898
alexeyYashin <alexeyyashin@mail.ru>
2023-02-25 23:20:05 +0400

    video06

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-     Hello, samurai! Let's go!!!!
+      <ul>
+        <li>css</li>
+        <li>html</li>
+        <li>js</li>
+        <li>React</li>
+      </ul>
     </div>
   );
 }
